ead74d351c677c9059393aed0bc3a8616388e0dc
konstantinos katrioplas <kokatrio@gmail.com>
2021-12-18 15:00:45 +0200

    configs, rust

--- .bashrc
+++ .bashrc
@@ -1,3 +1,4 @@
 alias vim="nvim"
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+. "$HOME/.cargo/env"
--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
 abbr -a e nvim
 abbr -a vim nvim
 if test -f /home/kon/.autojump/share/autojump/autojump.fish; . /home/kon/.autojump/share/autojump/autojump.fish; end
+set PATH /home/kon/.cargo/bin $PATH # until version 3.2.0 where there is fish_add_shell
